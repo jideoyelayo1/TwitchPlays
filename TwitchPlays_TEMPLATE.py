60b0c10e843ffd9344dd06a87a210aef9b3980e2
Jide Oyelayo <JideOyelayo1@Gmail.com>
2023-08-10 19:38:39 +0100

    update

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -9,14 +9,14 @@ from TwitchPlays_KeyCodes import *
 ##################### GAME VARIABLES #####################
 
 # Replace this with your Twitch username. Must be all lowercase.
-TWITCH_CHANNEL = 'angryginge13'   #'dougdougw'
+TWITCH_CHANNEL = 'angryginge13'  # 'dougdougw'
 
 # If streaming on Youtube, set this to False
 STREAMING_ON_TWITCH = True
 
 # If you're streaming on Youtube, replace this with your Youtube's Channel ID
 # Find this by clicking your Youtube profile pic -> Settings -> Advanced Settings
-YOUTUBE_CHANNEL_ID = "YOUTUBE_CHANNEL_ID_HERE" 
+YOUTUBE_CHANNEL_ID = "YOUTUBE_CHANNEL_ID_HERE"
 
 # If you're using an Unlisted stream to test on Youtube, replace "None" below with your stream's URL in quotes.
 # Otherwise you can leave this as "None"
@@ -35,7 +35,7 @@ MESSAGE_RATE = 0.5
 # This is helpful for games where too many inputs at once can actually hinder the gameplay.
 # Setting to ~50 is good for total chaos, ~5-10 is good for 2D platformers
 MAX_QUEUE_LENGTH = 20
-MAX_WORKERS = 100 # Maximum number of threads you can process at a time 
+MAX_WORKERS = 100  # Maximum number of threads you can process at a time
 
 last_time = time.time()
 message_queue = []
@@ -59,6 +59,7 @@ else:
     t = TwitchPlays_Connection.YouTube()
     t.youtube_connect(YOUTUBE_CHANNEL_ID, YOUTUBE_STREAM_URL)
 
+
 def handle_message(message):
     try:
         msg = message['message'].lower()
@@ -74,53 +75,57 @@ def handle_message(message):
 
         # I've added some example videogame logic code below:
 
-        ###################################
-        # Example GTA V Code 
-        ###################################
+        def GTA_V_Code(msg):
+            ###################################
+            # Example GTA V Code
+            ###################################
 
-        # If the chat message is "left", then hold down the A key for 2 seconds
-        if msg == "left": 
-            HoldAndReleaseKey(A, 2)
+            # If the chat message is "left", then hold down the A key for 2 seconds
+            if msg == "left":
+                HoldAndReleaseKey(A, 2)
 
-        # If the chat message is "right", then hold down the D key for 2 seconds
-        if msg == "right": 
-            HoldAndReleaseKey(D, 2)
+            # If the chat message is "right", then hold down the D key for 2 seconds
+            if msg == "right":
+                HoldAndReleaseKey(D, 2)
 
-        # If message is "drive", then permanently hold down the W key
-        if msg == "drive": 
-            ReleaseKey(S) #release brake key first
-            HoldKey(W) #start permanently driving
+            # If message is "drive", then permanently hold down the W key
+            if msg == "drive":
+                ReleaseKey(S)  # release brake key first
+                HoldKey(W)  # start permanently driving
 
-        # If message is "reverse", then permanently hold down the S key
-        if msg == "reverse": 
-            ReleaseKey(W) #release drive key first
-            HoldKey(S) #start permanently reversing
+            # If message is "reverse", then permanently hold down the S key
+            if msg == "reverse":
+                ReleaseKey(W)  # release drive key first
+                HoldKey(S)  # start permanently reversing
 
-        # Release both the "drive" and "reverse" keys
-        if msg == "stop": 
-            ReleaseKey(W)
-            ReleaseKey(S)
+            # Release both the "drive" and "reverse" keys
+            if msg == "stop":
+                ReleaseKey(W)
+                ReleaseKey(S)
 
-        # Press the spacebar for 0.7 seconds
-        if msg == "brake": 
-            HoldAndReleaseKey(SPACE, 0.7)
+            # Press the spacebar for 0.7 seconds
+            if msg == "brake":
+                HoldAndReleaseKey(SPACE, 0.7)
 
-        # Press the left mouse button down for 1 second, then release it
-        if msg == "shoot": 
-            pydirectinput.mouseDown(button="left")
-            time.sleep(1)
-            pydirectinput.mouseUp(button="left")
+            # Press the left mouse button down for 1 second, then release it
+            if msg == "shoot":
+                pydirectinput.mouseDown(button="left")
+                time.sleep(1)
+                pydirectinput.mouseUp(button="left")
 
-        # Move the mouse up by 30 pixels
-        if msg == "aim up":
-            pydirectinput.moveRel(0, -30, relative=True)
+            # Move the mouse up by 30 pixels
+            if msg == "aim up":
+                pydirectinput.moveRel(0, -30, relative=True)
 
-        # Move the mouse right by 200 pixels
-        if msg == "aim right":
-            pydirectinput.moveRel(200, 0, relative=True)
+            # Move the mouse right by 200 pixels
+            if msg == "aim right":
+                pydirectinput.moveRel(200, 0, relative=True)
 
-        ####################################
-        ####################################
+            ####################################
+            ####################################
+
+        ### GAMES ###
+        GTA_V_Code(msg)
 
     except Exception as e:
         print("Encountered exception: " + str(e))
@@ -130,11 +135,11 @@ while True:
 
     active_tasks = [t for t in active_tasks if not t.done()]
 
-    #Check for new messages
+    # Check for new messages
     new_messages = t.twitch_receive_messages();
     if new_messages:
-        message_queue += new_messages; # New messages are added to the back of the queue
-        message_queue = message_queue[-MAX_QUEUE_LENGTH:] # Shorten the queue to only the most recent X messages
+        message_queue += new_messages;  # New messages are added to the back of the queue
+        message_queue = message_queue[-MAX_QUEUE_LENGTH:]  # Shorten the queue to only the most recent X messages
 
     messages_to_handle = []
     if not message_queue:
@@ -161,5 +166,5 @@ while True:
             if len(active_tasks) <= MAX_WORKERS:
                 active_tasks.append(thread_pool.submit(handle_message, message))
             else:
-                print(f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
- 
+                print(
+                    f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
